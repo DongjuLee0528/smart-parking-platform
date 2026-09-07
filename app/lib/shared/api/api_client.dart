9a18d1c02233a478fd5147dc41bda3d38f73dc96
DongjuLee <dozoo0528@gmail.com>
2026-09-08 00:53:32 +0900

    chore: initialize project directory structure

---
+++ app/lib/shared/api/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:dio/dio.dart';
+
+final apiClient = Dio(BaseOptions(baseUrl: 'http://localhost:8080/api/v1'));
---
+++ service-server/src/main/java/com/smartparking/global/api/ApiResponse.java
@@ -0,0 +1,4 @@
+package com.smartparking.global.api;
+
+public record ApiResponse<T>(T data) {
+}
