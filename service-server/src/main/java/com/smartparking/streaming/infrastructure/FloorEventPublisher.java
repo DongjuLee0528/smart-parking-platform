5a012757a9561cdde894a39852ad52978363e0fa
DongjuLee <dozoo0528@gmail.com>
2026-09-11 21:00:28 +0900

    chore: initialize project directory structure

---
+++ service-server/src/main/java/com/smartparking/streaming/infrastructure/FloorEventPublisher.java
@@ -0,0 +1,4 @@
+package com.smartparking.streaming.infrastructure;
+
+public interface FloorEventPublisher {
+}
---
+++ service-server/src/test/java/com/smartparking/streaming/api/FloorEventControllerTest.java
@@ -0,0 +1,4 @@
+package com.smartparking.streaming.api;
+
+class FloorEventControllerTest {
+}
