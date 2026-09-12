5aaa549332cb8629c302bb961985da4553d31a2d
DongjuLee <dozoo0528@gmail.com>
2026-09-12 19:42:14 +0900

    chore: initialize project directory structure

---
+++ service-server/src/main/java/com/smartparking/floormap/dto/response/FloorMapSnapshotResponse.java
@@ -0,0 +1,4 @@
+package com.smartparking.floormap.dto.response;
+
+public record FloorMapSnapshotResponse() {
+}
---
+++ service-server/src/test/java/com/smartparking/floormap/unit/FloorMapServiceTest.java
@@ -0,0 +1,4 @@
+package com.smartparking.floormap.unit;
+
+class FloorMapServiceTest {
+}
