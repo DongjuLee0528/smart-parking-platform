ef1e9266300041cf216622962acaa2594c8cb6b4
DongjuLee <dozoo0528@gmail.com>
2026-09-13 13:48:02 +0900

    chore: initialize project directory structure

---
+++ app/test/features/authentication/login_page_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_parking_app/features/authentication/presentation/pages/login_page.dart';
+
+void main() {
+  group('LoginPage', () {
+    test('is declared', () {
+      expect(LoginPage, isNotNull);
+    });
+  });
+}
---
+++ service-server/src/main/java/com/smartparking/floormap/domain/MapVersionStatus.java
@@ -0,0 +1,7 @@
+package com.smartparking.floormap.domain;
+
+public enum MapVersionStatus {
+    DRAFT,
+    ACTIVE,
+    ARCHIVED
+}
