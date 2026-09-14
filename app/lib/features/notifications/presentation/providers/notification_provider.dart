fe2f9a4557e369d161f8f494a5b7864941c93a30
DongjuLee <dozoo0528@gmail.com>
2026-09-14 17:49:45 +0900

    chore: initialize project directory structure

---
+++ app/lib/features/notifications/presentation/providers/notification_provider.dart
@@ -0,0 +1,3 @@
+class NotificationProvider {
+  const NotificationProvider();
+}
---
+++ app/test/features/notifications/notification_permission_status_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_parking_app/features/notifications/presentation/widgets/notification_permission_status.dart';
+
+void main() {
+  group('NotificationPermissionStatus', () {
+    test('is declared', () {
+      expect(NotificationPermissionStatus, isNotNull);
+    });
+  });
+}
