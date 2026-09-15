fbaee5cb93f2b16249008f5fea3580c2d6e0e2be
DongjuLee <dozoo0528@gmail.com>
2026-09-15 22:11:15 +0900

    chore: initialize project directory structure

---
+++ ai-training/src/smart_parking_training/augmentation/parking_frame_augmentation.py
@@ -0,0 +1,2 @@
+class ParkingFrameAugmentation:
+    pass
---
+++ app/lib/features/parking_lot_details/presentation/widgets/parking_lot_availability_summary.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ParkingLotAvailabilitySummary extends StatelessWidget {
+  const ParkingLotAvailabilitySummary({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
