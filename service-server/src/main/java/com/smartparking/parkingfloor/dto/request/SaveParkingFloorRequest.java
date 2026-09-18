192a3ec2998c611d3be4d319eecd76eec7a32393
DongjuLee <dozoo0528@gmail.com>
2026-09-18 19:36:41 +0900

    chore: initialize project directory structure

---
+++ service-server/src/main/java/com/smartparking/parkingfloor/dto/request/SaveParkingFloorRequest.java
@@ -0,0 +1,4 @@
+package com.smartparking.parkingfloor.dto.request;
+
+public record SaveParkingFloorRequest() {
+}
---
+++ service-server/src/test/java/com/smartparking/savedparkinglocation/unit/SavedParkingLocationServiceTest.java
@@ -0,0 +1,4 @@
+package com.smartparking.savedparkinglocation.unit;
+
+class SavedParkingLocationServiceTest {
+}
