5aae59c91ce74464a08a4058354f773bf141c6f1
DongjuLee <dozoo0528@gmail.com>
2026-09-22 21:59:33 +0900

    chore: initialize project directory structure

---
+++ app/test/features/vehicle/vehicle_management_page_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smart_parking_app/features/vehicle/presentation/pages/vehicle_management_page.dart';
+
+void main() {
+  group('VehicleManagementPage', () {
+    test('is declared', () {
+      expect(VehicleManagementPage, isNotNull);
+    });
+  });
+}
